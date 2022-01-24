9df43e0864caf8aa5f6fbabb66155e6bda64c8ff
starunz <busouza01@gmail.com>
2022-01-24 16:09:35 -0500

    refactoring

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.use(cors());
 
 app.listen(5000);
 
-let user = [];
+const users = [];
 const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const signUpUser = req.body;
 
-    if (user.length === 0) {
-        user.push(signUpUser);
+    if (users.length === 0) {
+        users.push(signUpUser);
     } 
     else {
-        user = [signUpUser];
+        users = [signUpUser];
     }
 
     res.send('ok');
@@ -27,7 +27,7 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const tweet = req.body;
 
-    tweets.push({...tweet, avatar:user[0].avatar});
+    tweets.push({...tweet, avatar:users[0].avatar});
 
     res.send('ok');
 }); 
