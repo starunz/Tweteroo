c537067950ced2930ca8083739be12e7c37a6f0a
starunz <busouza01@gmail.com>
2022-01-24 12:00:42 -0500

    sign-up implementado

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express, { json } from 'express';
-import cors from 'cors'
+import cors from 'cors';
 
 const app = express();
 
-app.use(json())
+app.use(express.json());
 app.use(cors());
 
 app.listen(5000);
 
+const user = [];
+const tweet = [];
+
+app.post('/sign-up', (req, res) => {
+    const signUpUser = req.body;
+
+    user.push(signUpUser);
+
+    res.send('ok');
+});
